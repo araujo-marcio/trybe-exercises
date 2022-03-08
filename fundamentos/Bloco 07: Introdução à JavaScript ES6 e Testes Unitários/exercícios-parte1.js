90f7977e3a1385ebf438100d730ad4cad4d735b3
Marcio Araujo <araujo.marcio@pm.me>
2022-03-07 22:04:39 -0300

    Entrega da 1ª parte dos exercicios
    Exercicios de fixaçao da diferença entre var, let e const, transformar funçao em arrow function e usar template literal incluindo chamar uma varival na templeta literal.

---
+++ fundamentos/Bloco 07: Introdução à JavaScript ES6 e Testes Unitários/exercícios-parte1.js
@@ -0,0 +1,62 @@
+// Parte I
+// Agora você vai fazer alguns exercícios de fixação.
+// função original:
+/* function testingScope(escopo) {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true); */
+
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+function testingScope(escopo) {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  
+}
+
+testingScope(true);
+
+// Modifique a estrutura da função para que ela seja uma arrow function .
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  
+}
+testingScope(false);
+
+
+// Modifique as concatenações para template literals .
+
+ const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+    console.log(elseScope);
+  }
+ 
+}
+
+testingScope(true);
